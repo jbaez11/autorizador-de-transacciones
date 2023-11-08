f4acc88f4bdd5816233b18e55696b52aa954023c
Jonathan Baez <jbaez9910@gmail.com>
2023-11-07 23:22:25 -0500

    feat: Swagger Implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../src/shared/swagger/swaggerDefinition.json');
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 const createAccount = require('../src/usecases/createAccount');
 const authorizeTransaction = require('../src/usecases/authorizeTransaction');
@@ -19,6 +22,8 @@ app.post('/authorizeTransaction', (req, res) => {
   const account = authorizeTransaction.authorizeTransaction(id, comerciante, cantidad, tiempo);
   res.json(account);
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en http://localhost:${port}`);
